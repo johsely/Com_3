b79ca0b5751b876c466ff0183f06af2cecc83d97
chasling <c.haslinger91@hotmail.com>
2016-02-08 10:13:54 +0100

    a

--- MIECCompiler_VS_2015/MIECCompiler/Register.h
+++ MIECCompiler_VS_2015/MIECCompiler/Register.h
@@ -13,8 +13,18 @@
 
 class Register : public Object {
 public:
+	/**
+	* <summary>  Constructor. </summary>
+	*
+	* <param name="regNr">   eType. </param>
+	* <param name="size">  Size of the type basically 2. </param>
+	*/
 	Register(size_t regNr) : mRegNr(regNr) {}
 
+	/**
+	* <summary>  Getter for private mRegNr. </summary>
+	*
+	*/
 	size_t GetRegNumber() { return mRegNr; }
 
 private:
